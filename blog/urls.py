5857fe5a2abec66b928b5f310c11246a77894dc2
Sam LaGrave <srl0011@uah.edu>
2019-04-25 20:24:41 -0500

    Fixed urls & and HR

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('ranks/', views.rank_list, name='rank_list'),
-    path('player/<str:alias>', views.player_detail, name='player_detail'),
+    path('ranks/<str:alias>', views.player_detail, name='player_detail'),
 ]
